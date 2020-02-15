e41bf88c669e79a7bbeeafbc74a73e2ecd01929f
Noyan Alimov <noyan.alimov@gmail.com>
2020-02-15 18:52:25 +0000

    fix

--- server.js
+++ server.js
@@ -27,6 +27,6 @@ app.get('/profile/:id', (req, res) => { profile.handeProfile(req, res, db) })
 app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 2000, () => {
   console.log(`app is running on port ${process.env.PORT}`);
 })
